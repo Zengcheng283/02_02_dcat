590471215608e4dcc2887307ca608827ed52bd1e
Zengcheng283 <1265207567@qq.com>
2022-10-11 14:46:43 +0000

    change

--- dcat/bin/dcat.dart
+++ dcat/bin/dcat.dart
@@ -5,12 +5,35 @@ import 'package:process_run/shell.dart';
 
 Future<void> main(List<String> arguments) async {
   if (arguments.isEmpty) {
-  print_out("dcat \ttouch \tpath \t'create file'");
-  print_out("dcat \trm \tpath \t'remove file'");
-  print_out("dcat \ttext \t\t'print out words'");
+  print_out("touch \tpath \t'create file'");
+  print_out("rm \tpath \t'remove file'");
+  print_out("trash \tpath \t'remove file to trash'");
+  print_out("print \ttext \t'print out words'");
+  }
+  else if (arguments[0] == "trash") {
+    if (await FileSystemEntity.isDirectory(arguments[1])) {
+  print_out('error: ${arguments[1]} is a directory');
+}
+  else {
+    File file = File(arguments[1]);
+    if(await file.exists()) {
+      Directory dir = new Directory("/workspace/trash/");
+      dir.create();
+      file.copy("/workspace/personal_dcat/trash/${arguments[1]}");
+      file.delete();
+      }
+  }
   }
   else if (arguments[0] == "print") {
-    print_out(arguments[1]);
+    if (arguments[1].isEmpty) {
+      print_out("no input text");
+    }
+    else {
+      List list = arguments.toList();
+      list.removeRange(0, 1);
+    String s = list.join(" ");
+    print_out(s);
+    }
   }
   else if(arguments[0] == "rm") {
     if (await FileSystemEntity.isDirectory(arguments[1])) {
@@ -26,17 +49,11 @@ else if(arguments[0] == "touch") {
     if (await file.exists()) {
       print_out("File exists");
     }
-    else if(!arguments[1].contains(".")) {
-      print_out("This is dir, not file path!");
-    }
     else {
       File new_file = new File(arguments[1]);
       IOSink isk = new_file.openWrite();
       isk.close();
     }
-  }
-  else {
-    print_out(arguments[0]);
   }
 }
 
